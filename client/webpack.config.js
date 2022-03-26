3123ac020ccebe8587c81d8d76f08abf84f8607a
priscilla ma <priscillama621@gmail.com>
2022-03-26 17:33:30 -0400

    added code to webpack config.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,28 +3,75 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file.✅
+// TODO: Add CSS loaders and babel to webpack.✅
 
 module.exports = () => {
   return {
     mode: 'development',
+    // entry point for files
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      header: './src/js/header.js',
+      editor: './src/js/editor.js'
     },
+    //  OUTPUT FOR BUNDLES
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // webpack plugin that generates our html file and injects our bundles.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TXT EDITOR'
+      }),
+      // injects our custom service worker.
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      // creates manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'TXT EDITOR',
+        short_name: 'TXTEDIT',
+        description: 'write notes and code here!',
+        background_color: '#F2F2F2',
+        theme_color: '#4B6547',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
     ],
-
     module: {
+      // css loaders.
       rules: [
-        
-      ],
-    },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // use babel-loader in order to use es6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        }
+      ]
+    }
   };
 };
